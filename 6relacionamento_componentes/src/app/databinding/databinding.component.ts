af62c1f6dd85611fd6947e0c2d10adc0d65475c6
luizhenriquefernandes <luizhenrique.bio@gmail.com>
2023-07-13 11:14:53 -0300

    aula 8

--- 6relacionamento_componentes/src/app/databinding/databinding.component.ts
+++ 6relacionamento_componentes/src/app/databinding/databinding.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./databinding.component.scss']
 })
 export class DatabindingComponent {
+  //Aula 6
   /**É muito importante tipar a variável por boas práticas de código */
   nome: string = "Luís";
   anoDeNascimento: number = 2001;
+  
   /**cria uma variável e inclua a data e get full year e você poderá ser feliz]
    * E calcular a idade.
    */
@@ -17,9 +19,24 @@ export class DatabindingComponent {
   /**Este passo é foi criado um botão para deixar visível,
    * contudo se inserir true ele deixa o botão hidden.
   */
+ /**Aula 7 Criando botões */
   public checkoutDisable: boolean = false;
+  //criando botão que faz um alert
   public alertaInfo(){
     alert("Goku é Melhor que Naruto");
   }
+  /**Aula 8 criando botão e criando evento
+   * 
+  */
+ public alertaClica(valor: MouseEvent){
+  alert("Aula 8 em execução"+ valor);  
+ }
+
+ public position:{x: number, y: number}={x:0, y:0}
+ public moveMouse(valor: MouseEvent){
+  this.position.x = valor.offsetX;
+  this.position.y = valor.offsetY;
+ }
+ 
 
 }
